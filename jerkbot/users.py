2ddf678a2ce8590aed72a692f661e2359b9e0e7f
atvaccaro <atvaccaro@gmail.com>
2016-02-01 23:06:49 -0600

    omg

--- jerkbot/users.py
+++ jerkbot/users.py
@@ -7,7 +7,7 @@ class UserManager(object):
             users[row[0]] = row[1]
 
     def get_all_users():
-        return cursor.execute('SELECT * FROM user'):
+        return cursor.execute('SELECT * FROM user')
 
     def add_new_user(usercode, username, permission='user', beers=config.starting_beers):
         cursor.execute('INSERT INTO user VALUES (NULL, ?, ?, ?, ?)', (usercode, username, permission, beers))
