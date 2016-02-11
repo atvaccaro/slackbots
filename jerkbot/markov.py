a61cc4f15199ccce933394609b5b4392ace891f4
Saethlin <kimockb@gmail.com>
2016-02-11 00:12:05 -0500

    fixed period bug (I hope)

--- jerkbot/markov.py
+++ jerkbot/markov.py
@@ -8,7 +8,7 @@ class Markov(object):
     def __init__(self, usercode):
         message_history = ''
         for row in cursor.execute('SELECT body FROM message WHERE usercode=?', (usercode,)):
-            sentence = re.sub(r'\s', ' ', row[0])
+            sentence = re.sub(r'\s', ' ', row[0]).strip()
             if not sentence.endswith('.'):
                 sentence += '. '
             sentence = sentence[0].upper() + sentence[1:]
@@ -20,5 +20,8 @@ class Markov(object):
     def generate_markov_text(self):
         output = ''
         for r in range(config.markov_sentences):
-            output += ' '.join(self.text_model.chain.walk(None)) + ' '
+            sentence = ' '.join(self.text_model.chain.walk(None))
+            sentence = sentence.replace('.', '').strip()
+            output += sentence + '. '
+
         return output[:-1]
