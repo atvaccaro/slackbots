2d844d794d703c1e642f33e881a05f601ce85d3a
atvaccaro <atvaccaro@gmail.com>
2016-10-05 20:31:25 -0500

    move to example config

--- jerkbot/config.py
+++
@@ -1,9 +0,0 @@
-slack_token = 'xoxb-19648973122-JySsrhuJwEbI0F5VjzlNF6n1'
-slack_username = 'jerkbot'
-slack_usercode = 'idk yet'
-dbfile = 'jerkbot.db'
-reddit_user_agent = 'osx:com.atvaccaro.jerkbot:v0.1 (by /u/theplague42)'
-starting_beers = 3
-markov_sentences = 3
-markov_sentence_length = 7
-word_whitelist = ['LaTeX', 'John', 'Igor', 'Ben', 'Ethan', 'Andrew']
---
+++ jerkbot/example_config.py
@@ -0,0 +1,8 @@
+slack_token = 'slack_token'
+slack_username = 'slack_username'
+slack_usercode = 'slack_usercode'
+dbfile = 'path/to/db'
+reddit_user_agent = 'user_agent'
+markov_sentences = 3
+markov_sentence_length = 7
+word_whitelist = ['LaTeX', 'John', 'Igor', 'Ben', 'Ethan', 'Andrew']
